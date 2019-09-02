d168d814d429aa0f37a145270c519c8a7147878a
Nikolaos Tsipas <nikolaos.tsipas@skyscanner.net>
2019-09-02 21:33:21 +0100

    script fixes

--- backup.sh
+++ backup.sh
@@ -57,6 +57,6 @@ pg_dump $POSTGRES_HOST_OPTS $POSTGRES_DATABASE | gzip > dump.sql.gz
 
 echo "Uploading dump to $S3_BUCKET"
 
-aws s3 cp dump.sql.gz s3://$S3_BUCKET/$S3_PREFIX/${POSTGRES_DATABASE}_$(date +"%Y-%m-%dT%H:%M:%SZ").sql.gz
+AWS_ACCESS_KEY_ID=$S3_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$S3_SECRET_ACCESS_KEY aws s3 cp dump.sql.gz s3://$S3_BUCKET/$S3_PREFIX/${POSTGRES_DATABASE}_$(date +"%Y-%m-%dT%H:%M:%SZ").sql.gz
 
 echo "SQL backup uploaded successfully"
