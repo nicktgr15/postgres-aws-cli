59a60c900479fabce494db127396fe8bc4d2bf1d
Nikolaos Tsipas <nicktgr15@gmail.com>
2020-04-19 23:14:03 +0100

    Make port configurable

--- backup.sh
+++ backup.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-PGPASSWORD=${POSTGRES_PASSWORD} pg_dumpall -h ${POSTGRES_HOST} -p 5432 -U ${POSTGRES_USER} | gzip > dump.sql.gz
+PGPASSWORD=${POSTGRES_PASSWORD} pg_dumpall -h ${POSTGRES_HOST} -p ${POSTGRES_PORT} -U ${POSTGRES_USER} | gzip > dump.sql.gz
 
 AWS_ACCESS_KEY_ID=${S3_ACCESS_KEY_ID} AWS_SECRET_ACCESS_KEY=${S3_SECRET_ACCESS_KEY} aws s3 cp dump.sql.gz s3://${S3_BUCKET}/${S3_PREFIX}/all_dbs_$(date +"%Y-%m-%dT%H:%M:%SZ").sql.gz
 
